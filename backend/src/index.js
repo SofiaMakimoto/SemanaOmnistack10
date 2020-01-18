f04686f443c49cda51494ec8f7021c8847513db4
Sofia <sofiamakimoto@gmail.com>
2020-01-17 22:35:12 -0300

    Add web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes')
+const cors = require ('cors');
+const routes = require('./routes');
 
 const app = express();
 app.use(express.json());
@@ -9,8 +10,7 @@ mongoose.connect('mongodb+srv://soso:soso@cluster0-9z2ac.mongodb.net/week10?retr
     useUnifiedTopology: true
 });
 
-
+app.use(cors());
+app.use(express.json());
 app.use(routes);
-
-
 app.listen(3333);
