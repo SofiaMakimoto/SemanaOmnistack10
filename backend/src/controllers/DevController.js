6b2d046bbe6646892e1e7f0ad82b71f212672026
Sofia <sofiamakimoto@gmail.com>
2020-01-19 13:52:40 -0300

    add some advanced features

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringasArray = require('../utils/parseStringasArray');
+const { findConnections, sendMessage } = require  ('../websocket');
 
 module.exports = {
     async index(request, response) {
@@ -28,6 +29,13 @@ module.exports = {
                 techs: techsArray,
                 location
             });
+            //filtrar as conexões que estão há no máx 10 km de distancia
+            // e que o novo dev tenha pelo menps uma das techs filtradas
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techsArray
+            )
+           sendMessage(sendSocketMessageTo, 'new-dev', dev);
         }
         
         return response.json(dev);
